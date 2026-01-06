4e4ca037846068019a3d67a8a6e9c87c937b20cc
thomas <thomas@example.com>
2026-01-06 22:52:55 +0100

    :sparkles: feat: add xdg dirs

--- home/home.nix
+++ home/home.nix
@@ -15,6 +15,19 @@
     ./modules/desktop
   ];
 
+  # XDG directory configuration
+  xdg.userDirs = {
+    enable = true;
+    desktop = "${config.home.homeDirectory}/desktop";
+    documents = "${config.home.homeDirectory}/documents";
+    download = "${config.home.homeDirectory}/downloads";
+    music = "${config.home.homeDirectory}/music";
+    pictures = "${config.home.homeDirectory}/images";
+    videos = "${config.home.homeDirectory}/videos";
+    publicShare = "${config.home.homeDirectory}/public";
+    templates = "${config.home.homeDirectory}/.templates";
+  };
+
   programs.home-manager.enable = true;
 
   # Only user packages go here
