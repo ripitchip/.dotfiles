db6ce4f7587a7d04295ffbe0caa4f5f512564ae4
thomas <thomas@example.com>
2026-01-10 22:04:40 +0100

    :sparkles: feat: Update fish

--- home/modules/common/app/shell/fish.nix
+++ home/modules/common/app/shell/fish.nix
@@ -8,4 +8,16 @@
     '';
   };
   xdg.configFile."fish/functions/fish_prompt.fish".source = ./fish_prompt.fish;
+
+  # Declarative PNPM setup so pnpm doesn't need to edit config.fish
+  xdg.configFile."fish/conf.d/pnpm.fish".text = ''
+    set -gx PNPM_HOME "$HOME/.local/share/pnpm"
+    if not test -d $PNPM_HOME
+      mkdir -p $PNPM_HOME
+    end
+    # Ensure PNPM_HOME is on PATH (idempotent)
+    if not contains $PNPM_HOME $fish_user_paths
+      fish_add_path -Ua $PNPM_HOME
+    end
+  '';
 }
